d4de65acb57748f4000066faecd0e40d2665439e
Johnpaul Ochulor <johnpaulochulor@gmail.com>
2026-03-08 17:38:17 -0700

    Tested middleware

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ import helmet from 'helmet';
 import { PrismaClient } from '@prisma/client';
 
 const app = express();
-const prisma = new PrismaClient();
+// const prisma = new PrismaClient();
+const prisma = new PrismaClient({});
 
 // --- BASIC MIDDLEWARE SETUP ---
 app.use(helmet());        // Security headers
@@ -34,3 +35,19 @@ app.use((err, req, res, next) => {
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+// TESTING
+app.post('/api/test-json', (req, res) =>{
+console.log("Data recieved from Postman: ",
+req.body);
+
+if(!req.body  || Object.keys(req.body).length ===0){
+  return res.status(400).json({error: "JSON middleware failed - no data recieved"});
+}
+
+  res.json({
+    message: "Middleware is working!",
+    receivedData: req.body
+  });
+
+});
