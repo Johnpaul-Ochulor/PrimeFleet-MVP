81a24756955e8cc9f4f247ab66c9b56e0258074a
Johnpaul Ochulor <johnpaulochulor@gmail.com>
2026-03-17 10:05:44 -0700

    Updated edit

--- middleware/upload.js
+++ middleware/upload.js
@@ -4,7 +4,17 @@ const storage = multer.memoryStorage();
 
 const upload = multer({ 
     storage,
-    limits: { fileSize: 5 * 1024 * 1024 } // 5MB limit for stability
- });
+    limits: { fileSize: 5 * 1024 * 1024 }, // 5MB limit for stability
+  
+ fileFilter : (req, file, cb) => {
+    // Optional: Only allow images
+    if (file.mimetype.startsWith('image/')) {
+      cb(null, true);
+    } else {
+      cb(new Error('Only images are allowed!'), false);
+    }
+  }
+
+});
 
 export default upload;
